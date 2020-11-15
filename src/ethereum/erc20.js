3cc551d9b2beec4944804bf2a0cc27ac3bb5dc11
Bruno Calmels <bruno.calmels@macherit.com>
2020-11-15 19:08:50 -0300

    chore: ERC20 mock

--- src/ethereum/erc20.js
+++ src/ethereum/erc20.js
@@ -1,10 +1,10 @@
-// import ERC20Mock from "./ERC20Mock.json";
-import DocToken from "./DocToken.json";
+import ERC20Mock from "./ERC20Mock.json";
+// import DocToken from "./DocToken.json";
 import web3 from "./web3";
 
 export const contractInstance = (address) =>
-  new web3.eth.Contract(DocToken.abi, address);
-// new web3.eth.Contract(ERC20Mock.abi, address);
+  // new web3.eth.Contract(DocToken.abi, address);
+  new web3.eth.Contract(ERC20Mock.abi, address);
 
 export const allowance = async (instanceAddress, owner, spender) =>
   await contractInstance(instanceAddress)
